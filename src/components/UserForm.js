0b3b9961bd74823590bd5f2e56b7dedb442e0026
Skylarsj <skylarsj@icloud.com>
2023-04-11 11:38:52 -0700

    updated

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -3,15 +3,18 @@ import React, {useState} from 'react';
 
 
 const UserForm = (props) => {
-    const [username, setUsername] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     const createUser = (e) => {
 
         e.preventDefault();
 
-        const newUser = { username, email, password };
-        setUsername("");
+        const newUser = { firstName, lastName, email, password };
+        setFirstName("");
+        setLastName("");
         setEmail("");
         setPassword("");
 
@@ -19,21 +22,40 @@ const UserForm = (props) => {
     };
     
     return(
-        <form onSubmit={ createUser }>
+        <div>
+            <form onSubmit={ createUser }>
+                <div>
+                    <label>First Name: </label> 
+                    <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+                </div>
+                <div>
+                    <label>Last Name: </label> 
+                    <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+                </div>
+                <div>
+                    <label>Email Address: </label> 
+                    <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+                </div>
+                <div>
+                    <label>ConfirmPassword: </label>
+                    <input type="text" onChange={ (e) => setConfirmPassword(e.target.value) } />
+                </div>
+                <input type="submit" value="Create User" />
+            </form>
+            
             <div>
-                <label>Username: </label> 
-                <input type="text" onChange={ (e) => setUsername(e.target.value) } />
+                <p>Live Feed</p>
+                <p>First Name: {firstName}</p>
+                <p>Last Name: {lastName}</p>
+                <p>Email: {email}</p>
+                <p>Password: {password}</p>
+                <p>Confirm Password: {confirmPassword}</p>
             </div>
-            <div>
-                <label>Email Address: </label> 
-                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
-            </div>
-            <div>
-                <label>Password: </label>
-                <input type="text" onChange={ (e) => setPassword(e.target.value) } />
-            </div>
-            <input type="submit" value="Create User" />
-        </form>
+        </div>
     );
 };
 export default UserForm;
